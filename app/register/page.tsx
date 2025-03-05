51bd524d6dced8a745bb28b3435b88d9ceaabc72
Admir Bjelic <admir.bjelic@uzh.ch>
2025-03-05 23:47:40 +0100

    fixed linting errors.

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -5,7 +5,7 @@ import { useApi } from "@/hooks/useApi";
 import useLocalStorage from "@/hooks/useLocalStorage";
 import { User } from "@/types/user";
 import { Button, Form, Input } from "antd";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 // Optionally, you can import a CSS module or file for additional styling:
 // import styles from "@/styles/page.module.css";
 
@@ -25,7 +25,7 @@ const Register: React.FC = () => {
   // The hook returns an object with the value and two functions
   // Simply choose what you need from the hook:
   const {
-    value: token, // is commented out because we do not need the token value
+    // value: token, // is commented out because we do not need the token value
     set: setToken, // we need this method to set the value of the token to the one we receive from the POST request to the backend server API
     // clear: clearToken, // is commented out because we do not need to clear the token when logging in
   } = useLocalStorage<string>("token", ""); // note that the key we are selecting is "token" and the default value we are setting is an empty string
